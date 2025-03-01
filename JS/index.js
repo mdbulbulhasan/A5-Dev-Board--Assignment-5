24b112834a3b32d4fb30871709deb8d55db092b3
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2025-03-01 18:56:57 +0600

    Body background color change feature added done

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,25 @@
 // Route index.html to blogs.html
-document.getElementById("discoverNew").addEventListener("click", function (event) {
+document
+  .getElementById("discoverNew")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-  window.location.href = "blogs.html";
+    window.location.href = "blogs.html";
+  });
+// Route index.html to blogs.html
+// Full document background color Change Feature
+const rainbowColors = [
+  "rgba(255,0,0,0.8)",
+  "rgba(255,144,255,0.8)",
+  "rgba(255,255,98,0.8)",
+  "rgba(0,255,0,0.8)",
+  "rgba(0,140,255,0.8)",
+  "rgba(251,55,0,0.8)",
+  "rgba(87,255,255,0.8)",
+];
+let colorIndex = 0;
+document.getElementById("bg-change-btn").addEventListener("click", function () {
+  document.body.style.backgroundColor = rainbowColors[colorIndex];
+
+  colorIndex = (colorIndex + 1) % rainbowColors.length;
 });
+// Full document background color Change Feature
