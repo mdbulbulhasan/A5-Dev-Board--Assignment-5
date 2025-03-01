8ad66ea4d3a764f4f5d77ea6c20615426d6491b5
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2025-03-01 12:59:31 +0600

    Route blog page done

---
+++ JS/blogs.js
@@ -0,0 +1,5 @@
+// Route blogs.html to index.html 
+document.getElementById('backButton').addEventListener('click', function(event){
+    event.preventDefault();
+    window.location.href = 'index.html';
+})
---
+++ JS/index.js
@@ -0,0 +1,5 @@
+// Route index.html to blogs.html
+document.getElementById("discoverNew").addEventListener("click", function (event) {
+    event.preventDefault();
+  window.location.href = "blogs.html";
+});
